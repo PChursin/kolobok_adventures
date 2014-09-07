56bb7e254012f642f7d15a79ac668cc6fa13041f
Andrey Kokorev <a.d.kokorev@yandex.ru>
2014-09-07 20:13:17 +0400

    Soft body, v2

--- kolobok_adventures/Assets/Scripts/SoftBodyCreator.cs
+++ kolobok_adventures/Assets/Scripts/SoftBodyCreator.cs
@@ -10,45 +10,38 @@ public class SoftBodyCreator : MonoBehaviour {
 	public float h = 0;
 
 	// Use this for initialization
-	void Start () {		
+	void Start () {			
 		center = GameObject.CreatePrimitive (PrimitiveType.Sphere);
 		Component.DestroyImmediate (center.GetComponent<SphereCollider> ());
 		center.AddComponent<CircleCollider2D> ();
+
 		Rigidbody2D cBody = center.AddComponent<Rigidbody2D> ();
-		cBody.mass = 5;
+		cBody.mass = 5f;
+		//cBody.isKinematic = true;
+
 		center.transform.parent = gameObject.transform;
 		center.transform.localPosition = new Vector3 ();
 
 		spheres = new List<GameObject> ();
 
 		float angle = 0;
-		float R = 1.2f;
+		float R = 2;
 
 		while (angle < Mathf.PI * 2) {
 			GameObject g = GameObject.CreatePrimitive(PrimitiveType.Sphere);
 			Component.DestroyImmediate (g.GetComponent<SphereCollider>());
 			Rigidbody2D body = g.AddComponent<Rigidbody2D>();
-			body.mass = 0.01f;				
+			body.mass = 1f;				
 
+			DistanceJoint2D distanceJoint = g.AddComponent<DistanceJoint2D>();
+			distanceJoint.maxDistanceOnly = true;
+			distanceJoint.connectedBody = cBody;
+			distanceJoint.distance = R;
 
-			SpringJoint2D sprintJoint = g.AddComponent<SpringJoint2D> ();
-			sprintJoint.connectedBody = cBody;
-			sprintJoint.distance = R;
-			sprintJoint.frequency = 10;
-			sprintJoint.dampingRatio = 0f;
-
-
-			SliderJoint2D sliderJoint = g.AddComponent<SliderJoint2D>();
-			sliderJoint.angle = angle * Mathf.Rad2Deg;
-			sliderJoint.useLimits = true;
-			sliderJoint.collideConnected = true;
-		
-			JointTranslationLimits2D jtl = new JointTranslationLimits2D();
-			jtl.min = 0.1f;
-			jtl.max = R;
-			sliderJoint.limits = jtl;
-
-			sliderJoint.connectedBody = cBody;	
+			Spring s = g.AddComponent<Spring>();
+			s.connectedBody = cBody;
+			s.k = 400;
+			s.distance = R;
 
 			g.transform.localScale = new Vector3(0.2f, 0.2f, 1);
 			g.transform.parent = center.transform;
@@ -56,31 +49,19 @@ public class SoftBodyCreator : MonoBehaviour {
 
 			g.AddComponent<CircleCollider2D>();
 
-
 			spheres.Add(g);
 			angle += delta;
 		}
 
+
 		h = 2 * R * Mathf.Sin (delta / 2);
+
 		for (int i = 0; i < spheres.Count; i++) {
-			GameObject dummy = new GameObject();
-			Rigidbody2D dBody = dummy.AddComponent<Rigidbody2D>();
-			dBody.mass = 0;
-			dummy.transform.parent = spheres[i].transform;
-			dummy.transform.localPosition = new Vector3();
-
-			HingeJoint2D hingeJoint = dummy.AddComponent<HingeJoint2D>();
-			hingeJoint.connectedBody = spheres[i].GetComponent<Rigidbody2D>();
-
-			SpringJoint2D springJoint = dummy.AddComponent<SpringJoint2D>();
-			springJoint.connectedBody = spheres[ (i + spheres.Count - 1) % spheres.Count]
-										.GetComponent<Rigidbody2D>();
-			springJoint.distance = h;
-			springJoint.frequency = 100;
-			springJoint.dampingRatio = 0.5f;
+			DistanceJoint2D distanceJoint = spheres[i].AddComponent<DistanceJoint2D>();
+			distanceJoint.connectedBody = spheres[(i - 1 + spheres.Count) % spheres.Count]
+											.GetComponent<Rigidbody2D>();
+			distanceJoint.distance = h;
 		}
-
-
 	}
 	
 	// Update is called once per frame
